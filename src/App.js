919f33591cec99840a2c237b4e539d7ef4e0fed9
NicoleS1312 <102580592+nicole1312@users.noreply.github.com>
2022-06-24 17:19:03 +0100

    Removed image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import logo from "./logo.png";
 import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <h1>Dictionary</h1>
-      <div className="container logo-image">
-        <img src={logo} className="image-fluid app-logo" alt="logo" />
-      </div>
+
       <main>
         <Dictionary />
       </main>
