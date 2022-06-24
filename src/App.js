de4c1733637134f7b6b7aa1c764474f78df6fdff
NicoleS1312 <102580592+nicole1312@users.noreply.github.com>
2022-06-24 20:53:57 +0100

    Changed from rendering multiple to single meaning

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ function App() {
   return (
     <div className="App">
       <h1>Dictionary</h1>
-
       <main>
         <Dictionary />
       </main>
+      <footer>
+        <a
+          href="https://github.com/nicole1312/dictionary-project"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="source-code-link"
+        >
+          Open-souce-code
+        </a>
+        by Nicoleta Serban
+      </footer>
     </div>
   );
 }
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,19 +1,15 @@
 import React from "react";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h4>{props.meaning.partOfSpeech}</h4>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <h4>
-              <small>{definition.definition}</small>
-            </h4>
-            <p>{definition.example}</p>
-          </div>
-        );
-      })}
+      <div className="p-2 ">
+        <h4>{props.meaning.partOfSpeech}</h4>
+        <p>{props.meaning.definitions[0].definition}</p>
+
+        <em>{props.meaning.definitions[0].example}</em>
+      </div>
     </div>
   );
 }
