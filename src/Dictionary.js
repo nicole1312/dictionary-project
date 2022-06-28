b220afbf4d82d8894699a26a45ebd9db5d2d7722
NicoleS1312 <102580592+nicole1312@users.noreply.github.com>
2022-06-28 12:21:55 +0100

    Integrated photo API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [searchedWord, setSearchedWord] = useState(props.searchedWord);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     //documentation:https://dictionary.api.dev
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchedWord}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001e7a2f6aa8b57411b9d1d7bf234ca92be";
+    //documentation:https://www.pexels.com/api
+    let pexelsApiUrl = ` https://api.pexels.com/v1/search?query=${searchedWord}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -41,6 +53,7 @@ export default function Dictionary(props) {
           />
         </form>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <div className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a
+                  href={photo.src.original}
+                  terget="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={photo.src.landscape}
+                    className="image-fluid"
+                    alt="searched-word-illustration"
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
